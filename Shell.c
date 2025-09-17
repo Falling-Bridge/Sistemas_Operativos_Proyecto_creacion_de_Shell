3f89c23ae21975fbd107648b526fa8885305709e
Eddo <eddomora@gmail.com>
2025-09-17 18:10:50 -0300

    Creacion de char donde se almacena y despliega el comando a ejecutarse, junto a la estructura basica de ejecucion de comando

--- Shell.c
+++ Shell.c
@@ -6,26 +6,50 @@
 
 int main(int argc, char *argv[])
 {
-    int pid = fork();
-    if (pid < 0)
-    { // fork fallo
-        printf("fork fallo\n");
-        exit(1);
+    char escritura_comando[256]; // por la cantidad que admite un buffer
+    while (1)
+    {
+        printf("\n$ ");
+        fgets(escritura_comando, 256, stdin);
+        escritura_comando[strcspn(escritura_comando, "\n")] = 0;
+
+        char *args[256];
+        char *token = strtok(escritura_comando, " ");
+        int i = 0;
+        while (token != NULL)
+        {
+            args[i] = token;
+            token = strtok(NULL, " ");
+            i++;
+        }
+        args[i] = NULL;
+
+        int pid = fork();
+        if (pid < 0)
+        { // fork fallo
+            printf("fork fallo\n");
+            exit(1);
+        }
+        else if (pid == 0)
+        {
+            execvp(args[0], args);
+            /*
+            // Este ejemplo es de una ayudantia, pero se puede ocupar como base para poder avanzar
+            char *myargs[4]; // arreglo para almacenar el programa y sus argumentos
+            myargs[0] = strdup("wc");
+            myargs[1] = strdup("-l");
+            myargs[2] = strdup("fork0.c");
+            myargs[3] = NULL;
+            execvp(myargs[0], myargs); // cambia imagen de hijo a wc
+            printf("Aqui llegaría solo ante el error de execvp\n");
+            */
+        }
+        else
+        { // padre
+            int res = wait(NULL);
+        }
+        // printf("FIN proc actual %d\n", getpid());
     }
-    else if (pid == 0)
-    {                    // Este ejemplo es de una ayudantia, pero se puede ocupar como base para poder avanzar
-        char *myargs[4]; // arreglo para almacenar el programa y sus argumentos
-        myargs[0] = strdup("wc");
-        myargs[1] = strdup("-l");
-        myargs[2] = strdup("fork0.c");
-        myargs[3] = NULL;
-        execvp(myargs[0], myargs); // cambia imagen de hijo a wc
-        printf("Aqui llegaría solo ante el error de execvp\n");
-    }
-    else
-    { // padre
-        int res = wait(NULL);
-    }
-    printf("FIN proc actual %d\n", getpid());
+
     return 0;
 }
